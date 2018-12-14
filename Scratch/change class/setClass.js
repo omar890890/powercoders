013c6b37e8d75c0b7a05714a580dea529e032e85
Shadi Omar <shadi.omar@powercoders.org>
2018-12-14 15:42:02 +0000

    javascript optimizing

---
+++ Scratch/change class/setClass.js
@@ -0,0 +1,11 @@
+// javascript code goes here
+console.log('my code is running!');
+
+function greet(name) {
+  console.log('hello, ' + name);
+}
+
+function setClass(event, className) {
+       let el= document.getElementById('mypara');
+       el.className = className;
+}
