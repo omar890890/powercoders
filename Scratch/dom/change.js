af76d11da4fce2c80f7ccadbb0d70e717ff0b899
Shadi Omar <shadi.omar@powercoders.org>
2018-12-21 09:39:39 +0000

    create Para using Javascript

---
+++ Scratch/dom/change.js
@@ -0,0 +1,16 @@
+function x(event){
+    let el = document.getElementById('header');
+  el.className= 'tip';
+}
+
+document.write('hello world');
+
+document.getElementById("header").addEventListener("click", displayParagraph);
+
+function displayParagraph() {
+    let elP = document.createElement('p');
+    let content = document.createTextNode('This is my new paragraph');
+    elP.appendChild(content);
+    let elBody =document.getElementById('body');
+    elBody.appendChild(elP);
+}
