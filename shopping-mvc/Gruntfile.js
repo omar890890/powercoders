c82231f12303f09f76a848470c0ea80bab5b03cc
Shadi Omar <shadi.omar@powercoders.org>
2019-02-08 15:43:10 +0000

    copy static directory

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -1,25 +1,40 @@
 module.exports = function (grunt) {
-    //project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+  //project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 
-            },
-            build: {
-                src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
+      },
+      build: {
+        src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
 
-        }
+    },
+    copy: {
+      html: {
+        src: 'src/html/index.prod.html',
+        dest: 'build/index.html'
 
-    });
+      },
+      static: {
+        files: [{
+          expand: true,
+          cwd: 'src/static',
+          src: '*.*',
+          dest: 'build/static'
+        }]
+      }
+    }
+
+  });
 
 // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify-es');
-
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'copy']);
 
 };
