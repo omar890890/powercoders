903494e1189169575c5b8f4b28e82c090964ac35
Shadi Omar <shadi.omar@powercoders.org>
2019-02-14 14:43:52 +0000

    using grunt to modify css files through setting variables for repeated features.

--- shopping-mvc/Gruntfile.js
+++ shopping-mvc/Gruntfile.js
@@ -5,19 +5,16 @@ module.exports = function (grunt) {
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-
       },
       build: {
         src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
         dest: 'build/<%= pkg.name %>.min.js'
       }
-
     },
     copy: {
       html: {
         src: 'src/html/index.prod.html',
         dest: 'build/index.html'
-
       },
       static: {
         files: [{
@@ -27,14 +24,52 @@ module.exports = function (grunt) {
           dest: 'build/static'
         }]
       }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/css/style.min.css': ['src/css/*.css']
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: 'src/css/style.less',
+        tasks: 'less'
+      },
+      css: {
+        files: 'src/css/*.css',
+        tasks: ['cssmin']
+      },
+      scripts: {
+        files: 'src/js/*.js',
+        tasks: ['uglify']
+      },
+      html: {
+        files: 'src/html/index.prod.html',
+        tasks: ['copy:html']
+      },
+      static: {
+        files: 'src/static/*.*',
+        tasks: ['copy:static']
+      }
+    },
+    less: {
+      build: {
+        files: {
+          'src/style.css': 'src/css/style.less'
+        }
+      }
     }
-
   });
 
 // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-copy');
-// Default task(s).
-  grunt.registerTask('default', ['uglify', 'copy']);
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
+  // Default task(s).
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'copy']);
 };
