fc56fa3af7917039fbe73dd58785bd6538540da4
Shadi Omar <shadi.omar@powercoders.org>
2019-02-25 15:13:41 +0000

    Add event listener to the shopping list item to get the click button behavior.

--- shopping-react/src/ItemInput.js
+++ shopping-react/src/ItemInput.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 
 class ItemInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: '',
+      quantity: ''
+    };
+    this.onItemChange = this.onItemChange.bind(this);
+    this.onQuantityChange= this.onQuantityChange.bind(this)
+  }
+
+  onItemChange(event) {
+    this.setState({item: event.target.value}, () => console.log(this.state));
+  }
+
+  onQuantityChange(event) {
+    this.setState({quantity: event.target.value}, () => console.log(this.state));
+  }
+
   render() {
     return (
         <div>
           <label htmlFor="item"> Enter a new item:</label>
-          <input type="text" id="quantity" size="8" placeholder="quantity"/>
-          <input type="text" id="item" placeholder="type something to buy"/>
+          <input onChange={this.onQuantityChange} type="text" id="quantity" size="8" placeholder="quantity"/>
+          <input onChange={this.onItemChange} type="text" id="item" placeholder="type something to buy"/>
           <button id="add">Add item</button>
         </div>
     );
-    
   }
 }
 
